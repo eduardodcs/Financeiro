0fbe17768ee538b0f6db831b0a51ca15e802e876
eduardodcs <eddcesilva@gmail.com>
2021-07-21 02:34:31 -0300

    Implementação do Swagger para documentação

--- src/main/java/br/com/eduardo/financeiro/FinanceiroApplication.java
+++ src/main/java/br/com/eduardo/financeiro/FinanceiroApplication.java
@@ -3,7 +3,10 @@ package br.com.eduardo.financeiro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class FinanceiroApplication {
 
 	public static void main(String[] args) {
